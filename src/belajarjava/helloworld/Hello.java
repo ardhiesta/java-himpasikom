08593abca9d84e44e162adb79653c12e975068c8
ardhiesta <ardhi.mail@gmail.com>
2014-05-23 04:01:41 +0700

    tambah helloworld

---
+++ src/belajarjava/helloworld/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarjava.helloworld;
+
+/**
+ *
+ * @author linuxluv
+ */
+public class Hello {
+    /**
+        * My first java program
+    */
+    public static void main(String[] args) {
+        //menampilkan string ”Hello world” pada layar
+        System.out.println("Hello World");
+    }
+}
